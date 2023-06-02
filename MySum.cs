42078b57e95de353f0a145dd91a015fec480be63
MarkovAV <a.markov@mirtekgroup.ru>
2023-06-02 13:22:27 +0300

    Добавьте файлы проекта.

---
+++ MySum.cs
@@ -0,0 +1,103 @@
+﻿using System.Diagnostics;
+
+namespace hm8_parallelsum
+{
+    public class MySum
+    {
+        private int[] array;
+        public MySum(int[] array)
+        {
+            this.array = array;
+        }
+
+        public (long, TimeSpan) GetSequentialSum()
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            long sum = 0;
+            foreach (var item in array)
+            {
+                sum += item;
+            }
+            stopWatch.Stop();
+
+            return (sum, stopWatch.Elapsed);
+        }
+
+        public (long, TimeSpan) GetParallelLinqSum()
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            long sum = 0;
+            sum = array.AsParallel().Sum();
+
+            stopWatch.Stop();
+
+            return (sum, stopWatch.Elapsed);
+        }
+
+        public (long, TimeSpan) GetWithTaskSum(int taskCount) // taskCount - количество тасок, с помощью которых будет вычисляться сумма
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            long sum = 0;
+            var taskList = new List<Task<long>>();
+
+            // длина массива, для которого будет вычиляться сумма в таске
+            int subArrLen = array.Length / taskCount;
+            for (int i = 0; i < taskCount; i++)
+            {
+                int lastInd;
+
+                if (i == taskCount - 1)
+                {
+                    lastInd = array.Length;
+                }
+                else
+                {
+                    lastInd = (i + 1) * subArrLen;
+                }
+
+                taskList.Add(CalcSumInTaskAsync(i * subArrLen, lastInd));
+            }
+
+            try
+            {
+                Task.WaitAll(taskList.ToArray());
+                //var res = await t;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            //var res = await t;
+            foreach (var item in taskList)
+            {
+                sum = sum + item.Result;
+            }
+            stopWatch.Stop();
+            return (sum, stopWatch.Elapsed);
+        }
+
+        public async Task<long> CalcSumInTaskAsync(int firstInd, int lastInd)
+        {
+            var t = Task.Run(() =>
+            {
+                long sum = 0;
+
+                for (int i = firstInd; i <= lastInd - 1; i++)
+                {
+                    sum = sum + array[i];
+                }
+
+                return sum;
+
+            });
+
+            return await t;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿
+using hm8_parallelsum;
+
+
+SumCalc(100_000);
+SumCalc(1_000_000);
+SumCalc(10_000_000);
+
+static int[] GetRandomArray(int arrLenght)
+{
+    int[] arr = new int[arrLenght];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(-1000, 1000);
+    }
+
+    return arr;
+}
+
+static async void SumCalc(int arrLenght)
+{
+    int[] arr = GetRandomArray(arrLenght);
+    MySum mySum = new MySum(arr);
+
+
+    Console.WriteLine("Последовательное суммирование " + arrLenght + " элементов:");
+    var ressum = mySum.GetSequentialSum();
+    Console.WriteLine("Сумма: {0}, время: {1}", ressum.Item1, ressum.Item2);
+    Console.WriteLine();
+
+    Console.WriteLine("Параллельное суммирование с помощью LINQ " + arrLenght + " элементов:");
+    ressum = mySum.GetParallelLinqSum();
+    Console.WriteLine("Сумма: {0}, время: {1}", ressum.Item1, ressum.Item2);
+    Console.WriteLine();
+
+    Console.WriteLine("Параллельное суммирование с помощью Task " + arrLenght + " элементов:");
+    ressum = mySum.GetWithTaskSum(11);
+    Console.WriteLine("Сумма: {0}, время: {1}", ressum.Item1, ressum.Item2);
+
+    Console.WriteLine();
+    Console.WriteLine("----------------------------------------------------------------------");
+    Console.WriteLine();
+}
+
